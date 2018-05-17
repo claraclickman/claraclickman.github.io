d2863abeefacb42915e81e464cbc043f2b5daff3
claraclickman <34690758+claraclickman@users.noreply.github.com>
2018-05-17 17:39:09 -0400

    merr

--- js/random.js
+++ js/random.js
@@ -2,10 +2,10 @@ function randombg() {
     var random = Math.floor(Math.random() * 6) + 0;
     var bigSize = ["radial-gradient(circle at 50% 50%, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.88)), url('https://raw.githubusercontent.com/claraclickman/claraclickman.github.io/master/Images/Kitchen2_1.jpg')",
         "radial-gradient(circle at 50% 50%, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.88)), url('https://raw.githubusercontent.com/claraclickman/claraclickman.github.io/master/Images/alhambra_ceiling.jpg')",
-        "radial-graient(circle at 50% 50%, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.88)), url('https://raw.githubusercontent.com/claraclickman/claraclickman.github.io/master/Images/cloudcity.png')",
+        "radial-gradient(circle at 50% 50%, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.88)), url('https://raw.githubusercontent.com/claraclickman/claraclickman.github.io/master/Images/cloudcity.png')",
         "radial-gradient(circle at 50% 50%, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.88)), url('https://raw.githubusercontent.com/claraclickman/claraclickman.github.io/master/Images/josh_kline_full.jpg')",
         "url('http://placehold.it/300&text=banner5')",
         "url('http://placehold.it/300&text=banner6')"
     ];
-    document.getElementsByClassName("intro").style.backgroundImage = bigSize[random];
+    document.getElementsByClassName("intro").style.backgroundImage = bigSize[3];
 }
